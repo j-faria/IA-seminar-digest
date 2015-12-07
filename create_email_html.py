ad4dd8122f18874697de2c8acbaa5d4120e5f0e6
João Faria <joao.faria@astro.up.pt>
2015-12-07 13:58:06 +0000

    programmers club only in Porto

--- create_email_html.py
+++ create_email_html.py
@@ -11,8 +11,6 @@ weekdates = start.strftime('%d/%b/%Y') + ' -- ' + end.strftime('%d/%b/%Y')
 fileid = 'WeekDigest.' + start.strftime('%d%b%Y') + '.' + end.strftime('%d%b%Y') + '.html'
 
 
-with open('email_template.html') as f:
-    email_template = f.read()
 
 
 google_calendar_link = 'http://www.google.com/calendar/render?action=TEMPLATE'
@@ -58,51 +56,53 @@ while True:
 
     print '\n'
 
+    iteration += 1
+
 
 email_template = email_template.replace('{{seminars}}', seminar_content)
 email_template = email_template.replace('{{weekdates}}', weekdates)
 
+if IA_pole == 'Porto':
+    progclub_content = ''
+    PC = raw_input('Is there a programmers club this week (y/[n]) ')
+    if PC == 'y':
 
-progclub_content = ''
-PC = raw_input('Is there a programmers club this week (y/[n]) ')
-if PC == 'y':
+        progclub = '\n<p class="lead">Also this week, there will be the Programmers Club:</p>\n'
 
-    progclub = '\n<p class="lead">Also this week, there will be the Programmers Club:</p>\n'
+        datein = raw_input('Date of the programmers club (DD-MM-YYYY): ')
 
-    datein = raw_input('Date of the programmers club (DD-MM-YYYY): ')
+        title, presenter, abstract, startime, dtstart, location, icslink = get_info_from_gcalendar(datein, IA_pole)
+        link = ''
 
-    title, presenter, abstract, startime, dtstart, location, icslink = get_info_from_gcalendar(datein)
-    link = ''
+        progclub_content += '<h9><b>%s</b></h9> <br />\n' % title
+        progclub_content += '<h9>%s</h9> <br />\n' % presenter
+        progclub_content += '<p>%s</p>\n' % abstract
+        progclub_content += '<br />\n'
+        progclub_content += '<i>%s, %s</i>\n' % (location, startime)
+        progclub_content += '<br />\n'
 
-    progclub_content += '<h9><b>%s</b></h9> <br />\n' % title
-    progclub_content += '<h9>%s</h9> <br />\n' % presenter
-    progclub_content += '<p>%s</p>\n' % abstract
-    progclub_content += '<br />\n'
-    progclub_content += '<i>%s, %s</i>\n' % (location, startime)
-    progclub_content += '<br />\n'
+        datestart = time.strftime('%Y%m%dT%H%M00Z', dtstart)
+        end = datetime(*dtstart[:6])+timedelta(hours=1)
+        dateend = end.strftime('%Y%m%dT%H%M00Z')
 
-    datestart = time.strftime('%Y%m%dT%H%M00Z', dtstart)
-    end = datetime(*dtstart[:6])+timedelta(hours=1)
-    dateend = end.strftime('%Y%m%dT%H%M00Z')
+        link = google_calendar_link % (title, datestart, dateend, presenter, location)
+        # print link
 
-    link = google_calendar_link % (title, datestart, dateend, presenter, location)
-    # print link
+        progclub_content += 'Save this event to your calendar:\n'
+        # progclub_content += '<a href="%s">Outlook</a> -- \n' % icslink
+        # progclub_content += '<a href="%s">iCalendar</a> -- \n' % icslink
+        progclub_content += '<a href="%s">Google Calendar</a>\n' % link
 
-    progclub_content += 'Save this event to your calendar:\n'
-    # progclub_content += '<a href="%s">Outlook</a> -- \n' % icslink
-    # progclub_content += '<a href="%s">iCalendar</a> -- \n' % icslink
-    progclub_content += '<a href="%s">Google Calendar</a>\n' % link
+        progclub_content += '<hr />\n'
+        progclub_content += '<hr />\n'
+        progclub_content += '<br />\n'
+        print '\n'
 
-    progclub_content += '<hr />\n'
-    progclub_content += '<hr />\n'
-    progclub_content += '<br />\n'
-    print '\n'
+        progclub_content = progclub + progclub_content
+        email_template = email_template.replace('{{programmersclub}}', progclub_content)
 
-    progclub_content = progclub + progclub_content
-    email_template = email_template.replace('{{programmersclub}}', progclub_content)
-
-else:
-    email_template = email_template.replace('{{programmersclub}}', '\n')
+    else:
+        email_template = email_template.replace('{{programmersclub}}', '\n')
 
 # print email_template
 
